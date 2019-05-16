67fc6245e785f1afe476b164e03347c2280e0043
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-05-16 13:32:55 -0600

    commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express = require("express");
-
+const massive = require('massive')
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING).then(db => {app.set('db',db)
+  // db.new_planes().then(planes => console.log( planes ))
+  // .catch( err => console.log(err))
+
+})
 
 app.use(express.json());
 
